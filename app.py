485ffa177ee3d3e348744e9469835fe1fadcbf8c
gbalaratnaswamy <gbalaratnaswamy@gmail.com>
2020-12-14 20:00:45 +0530

    ml added

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 # sudoku solver
 #
 import cv2
+from wraper.sudoku_wrapper import wrap
+from ml_detector.sudoku_digitextract import *
+from libraries import sudokusolver
+
 print("""
 ############################################################################
                         sudoku solver
 ############################################################################""")
-path = input("enter image path : ")
-sudoku_img =cv2.imread(path)
+path = "sudoku3.png"
+sudoku_img = cv2.imread(path)
+img = wrap(path)
+cv2.imshow("img",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+sudoku = clean_image(sudoku_img)
+sudoku_matrix, sudoku_clean = predict_digits(sudoku)
+print(sudoku_matrix)
+sudoku_matrix = sudokusolver.solve_sudoku(sudoku_matrix)
+print(sudoku_matrix)
